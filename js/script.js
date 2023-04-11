26b7a59687e3f726ac9400430504b9bb546c926f
turjaSaha <turjasaha800@gmail.com>
2023-04-11 19:14:47 +0600

    change 2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,68 @@
-$(function(){
-   // about slicl slider
+$(function () {
+  // about slick slider
 
-    $('.about_slider').slick({
-        infinite: true,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        arrows: false,
-        
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 400,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
+  $('.about_slider').slick({
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // about slick slider end
+
+  //back to top
+
+  $(window).on('scroll', function () {
+
+  var winscroll = $(window).scrollTop();
+
+    if (winscroll > 1000) {
+      $('.backtop p').fadeIn(1000);
+    }else{
+      $('.backtop p').fadeOut(1000);
+    }
+  });
+
+  $('.backtop').on('click',function(){
+    $('html,body').animate({
+      scrollTop: 0  
+      },1000);
+  });
+
+});
+
+
+// preloader js
+
+$(function () {
+  $(window).on('load', function () {
+    $(".preloader_main").delay(1000).fadeOut(1000);
+  });
 });
